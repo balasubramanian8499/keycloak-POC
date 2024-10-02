049eae7ccadf12a496e87ad406f38cf1e70d38bb
Balasubramanian S <86341262+balasubramanian8499@users.noreply.github.com>
2024-10-02 15:38:29 +0530

    Adding src/main/java/com/example/keycloak/keycloakconfig/TokenDto.java

---
+++ src/main/java/com/example/keycloak/keycloakconfig/TokenDto.java
@@ -0,0 +1,15 @@
+package com.example.keycloak.keycloakconfig;
+
+public class TokenDto {
+
+    private String token;
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+}

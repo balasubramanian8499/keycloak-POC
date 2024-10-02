ed9059eaeb83bc76ba4d9173fc88579b9c78f48b
Balasubramanian S <86341262+balasubramanian8499@users.noreply.github.com>
2024-10-02 15:38:29 +0530

    Adding src/main/java/com/example/keycloak/model/User.java

---
+++ src/main/java/com/example/keycloak/model/User.java
@@ -0,0 +1,30 @@
+package com.example.keycloak.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "user")
+public class User {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "id")
+  private Integer id;
+
+  @Column(name = "username")
+  private String username;
+
+  @Column(name = "password")
+  private String password;
+
+  @Column(name = "kuid")
+  private String kuid;
+
+  @Column(name = "email")
+  private String email;
+
+}

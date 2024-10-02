910f65137dbad677c6ba4a36b735184beee748b0
Balasubramanian S <86341262+balasubramanian8499@users.noreply.github.com>
2024-10-02 15:38:21 +0530

    Adding src/main/java/com/example/keycloak/exception/ResponseEntityBuilder.java

---
+++ src/main/java/com/example/keycloak/exception/ResponseEntityBuilder.java
@@ -0,0 +1,12 @@
+package com.example.keycloak.exception;
+
+import org.springframework.http.ResponseEntity;
+public class ResponseEntityBuilder {
+
+	public static ResponseEntity<Object> build(ApiError apiError){
+
+		return new ResponseEntity<>( apiError, apiError.getStatus( ) );
+	}
+
+}
+
